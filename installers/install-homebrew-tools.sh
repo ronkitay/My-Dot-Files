9c9ef6f10d884d35eda0592c818cf984d0df691f
Ron Kitay <ronkitay@gmail.com>
2024-09-22 08:42:07 +0300

    installer improvements

--- installers/install-homebrew-tools.sh
+++ installers/install-homebrew-tools.sh
@@ -1,76 +1,56 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(dirname "$0")
+HOMEBREW_HOME=/opt/homebrew/bin
 
 function logStep() {
   echo "${BRIGHT}${GREEN}Step ${CURRENT_STEP}/${STEP_COUNT}: $1${NORMAL}"
   let CURRENT_STEP=CURRENT_STEP+1
 }
 
-function installFormulae() {
-    if [ -f /opt/homebrew/bin/$1 ]; 
+function installOrUpdateHomebrew() {
+    logStep "Installing/Updating Homebrew - https://brew.sh/"
+    if [ -f ${HOMEBREW_HOME}/brew ]; 
     then
-        brew upgrade $1;
+        brew update;
     else
-        brew install $1;
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
 }
 
-STEP_COUNT=16
+function installOrUpdateFormulae() {
+    title=$1
+    url=$2
+    formula=$3
+    tap=$4
 
+    logStep "Installing/Updating $title ($formula) - $url"
+
+    if [ -n "$tap" ]; 
+    then
+        [[ $(brew tap $tap > /dev/null 2>&1) -ne 0 ]] && brew tap $tap
+    fi
+
+    if [ -f ${HOMEBREW_HOME}/$formula ]; 
+    then
+        brew upgrade $formula;
+    else
+        brew install $formula;
+    fi
+}
+
+
+input_file="${SCRIPT_DIR}/homebrew-tools.csv"
+let STEP_COUNT="$(cat $input_file | egrep -v '^\s+$'| wc -l)"+2
 let CURRENT_STEP=1
 
-logStep "Installing/Updating Homebrew - https://brew.sh/"
-if [ -f /opt/homebrew/bin/brew ]; 
-then
-    brew update;
-else
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
+installOrUpdateHomebrew
 
-logStep "Installing/Updating Fuzzy Finder (fzf) - https://github.com/junegunn/fzf"
-installFormulae fzf
-
-logStep "Installing/Updating batcat (bat) - https://github.com/sharkdp/bat"
-installFormulae bat
-
-logStep "Installing/Updating fd (find replacement) - https://github.com/sharkdp/fd"
-installFormulae fd
-
-logStep "Installing/Updating jq (JSON Query)  - https://jqlang.github.io/jq/download/"
-installFormulae jq
-
-logStep "Installing/Updating yq (YAML Query)  - https://github.com/mikefarah/yq"
-installFormulae yq
-
-logStep "Installing/Updating tree - https://formulae.brew.sh/formula/tree"
-installFormulae tree
-
-logStep "Installing/Updating taskwarrior - https://taskwarrior.org/download/"
-installFormulae task
-
-logStep "Installing/Updating SwitchAudioSource  - https://github.com/deweller/switchaudio-osx"
-installFormulae switchaudio-osx
-
-logStep "Installing/Updating asdf - https://asdf-vm.com/guide/getting-started.html"
-installFormulae asdf
+while IFS=';' read -r title url formula tap || [ -n "$title" ]; do
+    [ -z "$title" ] && continue
+    
+    installOrUpdateFormulae "$title" "$url" "$formula" "$tap"
+done < "$input_file"
 
 logStep "Installing/Updating asdf plugins - ${SCRIPT_DIR}/install-asdf-plugins.sh"
 ${SCRIPT_DIR}/install-asdf-plugins.sh
-
-logStep "Installing/Updating kubectl - https://kubernetes.io/docs/tasks/tools/install-kubectl-macos/#install-with-homebrew-on-macos"
-installFormulae kubectl
-
-logStep "Installing/Updating kubectx - https://formulae.brew.sh/formula/kubectx"
-installFormulae kubectx
-
-logStep "Installing/Updating helm - https://helm.sh/docs/intro/install/"
-installFormulae helm
-
-logStep "Installing/Updating clisso - https://github.com/allcloud-io/clisso"
-[[ $(brew tap allcloud-io/tools > /dev/null 2>&1) -ne 0 ]] && brew tap allcloud-io/tools
-installFormulae clisso
-
-logStep "Installing/Updating granted - https://docs.commonfate.io/granted/getting-started"
-[[ $(brew tap common-fate/granted > /dev/null 2>&1) -ne 0 ]] && brew tap common-fate/granted
-installFormulae granted
