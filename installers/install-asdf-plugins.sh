3c139d311723073ed82d5ea98fa81705e52b38ad
Ron Kitay <ronkitay@gmail.com>
2024-11-05 09:54:03 +0200

    Configure ASDF plugins for ronk mac and support ASDF plugins with custom URLs like Terraform

--- installers/install-asdf-plugins.sh
+++ installers/install-asdf-plugins.sh
@@ -16,12 +16,18 @@ function logStep() {
 function installPlugin() {
   title=$1
   plugin=$2
+  url=$3
 
   let PLUGIN_COUNT=$(asdf plugin list | egrep "^${plugin}$" | wc -l|bc)
   if [ $PLUGIN_COUNT -eq 0 ]; 
   then
     logStep "Installing plugin ${title}"
-    asdf plugin add $plugin
+    if [ -z "$url" ]; 
+    then
+      asdf plugin add $plugin
+    else
+      asdf plugin-add $plugin $url
+    fi
   else
     logStep "Updating plugin ${title}"
     asdf plugin update $plugin
@@ -44,17 +50,50 @@ function setGlobalVersion() {
   asdf global $plugin $version
 } 
 
-if [ ! -f ${ASDF_PLUGIN_CONFIG_FILE} ]; 
-then
-  echo "${BRIGHT}Could not find the config file ${RED}${ASDF_PLUGIN_CONFIG_FILE}${WHITE} - aborting"
-  exit 1
-fi
+function validateConfiguration() {
+  if [ ! -f ${ASDF_PLUGIN_CONFIG_FILE} ]; 
+  then
+    echo "${BRIGHT}Could not find the config file ${RED}${ASDF_PLUGIN_CONFIG_FILE}${WHITE} - aborting"
+    exit 1
+  fi
 
-if [ ! -f ${ASDF_VERSIONS_CONFIG_FILE} ]; 
-then
-  echo "${BRIGHT}Could not find the config file ${RED}${ASDF_VERSIONS_CONFIG_FILE}${WHITE} - aborting"
-  exit 1
-fi
+  if [ ! -f ${ASDF_VERSIONS_CONFIG_FILE} ]; 
+  then
+    echo "${BRIGHT}Could not find the config file ${RED}${ASDF_VERSIONS_CONFIG_FILE}${WHITE} - aborting"
+    exit 1
+  fi
+}
+
+function installPlugins() {
+  while IFS=';' read -r title plugin url || [ -n "$title" ]; do
+      [ -z "$title" ] && continue
+      
+      installPlugin "$title" "$plugin" "$url"
+  done < "$ASDF_PLUGIN_CONFIG_FILE"
+}
+
+function installVersions() {
+  while IFS=';' read -r plugin version || [ -n "$plugin" ]; do
+    [ -z "$plugin" ] && continue
+    [ -z "$version" ] && continue
+    
+    installVersion "$plugin" "$version"
+  done < "$ASDF_VERSIONS_CONFIG_FILE"
+}
+
+function setGlobals() {
+  if [ -f ${ASDF_GLOBALS_CONFIG_FILE} ]; 
+  then
+    while IFS=';' read -r plugin version || [ -n "$plugin" ]; do
+        [ -z "$plugin" ] && continue
+        [ -z "$version" ] && continue
+        
+        setGlobalVersion "$plugin" "$version"
+    done < "$ASDF_GLOBALS_CONFIG_FILE"
+  fi
+}
+
+validateConfiguration
 
 let STEP_COUNT="$(cat $ASDF_PLUGIN_CONFIG_FILE | egrep -v '^\s+$'| wc -l)+$(cat $ASDF_VERSIONS_CONFIG_FILE | egrep -v '^\s+$'| wc -l)"
 
@@ -63,60 +102,10 @@ then
   let STEP_COUNT="$(cat $ASDF_GLOBALS_CONFIG_FILE | egrep -v '^\s+$'| wc -l)+${STEP_COUNT}"
 fi
 
+installPlugins
 
-while IFS=';' read -r title plugin || [ -n "$title" ]; do
-    [ -z "$title" ] && continue
-    
-    installPlugin "$title" "$plugin"
-done < "$ASDF_PLUGIN_CONFIG_FILE"
+installVersions
+
+setGlobals
 
 
-while IFS=';' read -r plugin version || [ -n "$plugin" ]; do
-    [ -z "$plugin" ] && continue
-    [ -z "$version" ] && continue
-    
-    installVersion "$plugin" "$version"
-done < "$ASDF_VERSIONS_CONFIG_FILE"
-
-if [ -f ${ASDF_GLOBALS_CONFIG_FILE} ]; 
-then
-  while IFS=';' read -r plugin version || [ -n "$plugin" ]; do
-      [ -z "$plugin" ] && continue
-      [ -z "$version" ] && continue
-      
-      setGlobalVersion "$plugin" "$version"
-  done < "$ASDF_GLOBALS_CONFIG_FILE"
-fi
-
-# logStep "Installing Java"
-# installPlugin java
-# asdf global java system
-# asdf install java openjdk-21
-
-# logStep "Installing Kotlin"
-# installPlugin kotlin
-# asdf install kotlin 1.9.23
-# asdf global kotlin 1.9.23
-
-# logStep "Installing Python"
-# installPlugin python
-# asdf install python 3.10.10
-# asdf global python 3.10.10
-
-# logStep "Installing NodeJS"
-# installPlugin nodejs
-# asdf install nodejs 14.21.3
-# asdf install nodejs 16.16.0
-# asdf install nodejs 20.11.1
-
-# logStep "Installing Golang"
-# installPlugin golang
-# asdf install golang 1.21.1
-# asdf global golang 1.21.1
-
-# logStep "Installing Terraform"
-# # addPlugin terraform https://github.com/asdf-community/asdf-hashicorp.git
-# addPlugin terraform https://github.com/echo asdf-community/echo asdf-hashicorp.git
-# asdf install terraform 0.13.7
-# asdf install terraform 1.5.4
-# asdf global terraform 0.13.7
